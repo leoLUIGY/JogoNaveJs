ae8d2e302870f72509381465da7168d4e32fbf5c
leonidas <leonidas200999@hotmail.com>
2021-01-19 17:22:45 -0300

    add image to nav and rock

--- nave.js
+++ nave.js
@@ -6,19 +6,29 @@ let posBala;
 let posCom;
 let tiro = false;
 let newCometa = false;
+let gameActive = true;
+
 
 let c =  document.getElementById('canvas');
 
 let ctx = c.getContext("2d");
 
-
+let imgMet = 'Meteoro.jpeg';
+let imgNav = 'frente.jpeg';
+let imgFun = 'bkground.jpeg';
+let navObj = new Image();
+let metObj = new Image();
+let funObj = new Image();
+navObj.src = imgNav;
+metObj.src = imgMet;
+funObj.src = imgFun;
 function andar(tecla){
     if(tiro){
         new bala();
         tiro = false;
     }
-    ctx.fillStyle='red';
-    ctx.fillRect(posisX, posisY, 50, 50);
+
+    ctx.drawImage(navObj, posisX, posisY,80, 80);
 
     if(tecla == 32 && tiro == false)tiro = true;
     if(tecla == 37)posisX-=10;
@@ -29,23 +39,28 @@ function andar(tecla){
 
 }
 function cometas(){
-    ctx.fillStyle='black';
+   
     if(newCometa == true){
         posCom = [Math.floor(Math.random() * 1000),0];
-        ctx.fillRect(posCom[0], posCom[1], 50, 50);
     } else{
-        ctx.fillRect(posCom[0], posCom[1], 50, 50);
         posCom[1] += 2;
     }
+    if((posCom[0]+25 >= posisX+10 && posCom[0]+25<=posisX + 70)&&(posCom[1]>=posisY + 5)){
+        gameActive = false;
+    }
+    
+    ctx.drawImage(metObj,posCom[0], posCom[1], 50, 50);
     setTimeout('cometas()', '2');
 }
 function ativarCometa(){
-    newCometa =true;
-    if(newCometa){
-        new cometas();
-        newCometa = false;
+    if(gameActive){
+        newCometa =true;
+        if(newCometa){
+            new cometas();
+            newCometa = false;
+        }
+        setTimeout('ativarCometa()', '1500');
     }
-    setTimeout('ativarCometa()', '1500');
 }
 
 
@@ -62,10 +77,13 @@ function bala(){
     setTimeout('bala()', '2');
 }
 function Game(){
+    if(gameActive){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.drawImage(funObj, 0, 0);
     andar();
 
     setTimeout('Game()', '2');
+    } 
 }
 ativarCometa();
 Game();
