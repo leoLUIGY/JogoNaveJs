297a7403505f563b95343091ed596a2435584350
leonidas <leonidas200999@hotmail.com>
2021-02-08 23:12:02 -0300

    system a little better

--- nave.js
+++ nave.js
@@ -11,44 +11,61 @@ restart.addEventListener('click', function(){
 });
 
 start.addEventListener('click', StartGame);
-
+//=============================VARIAVEIS ==============================================
+    //===============VARIAVEIS DE INICIALIZAÇÃO===========================
     let balaPontos = 0;
+    let distancia = 0;
     let gameActive = true;
     let jogo;
     let tecla;
-
-
     let c =  document.getElementById('canvas');
     let ctx = c.getContext("2d");
 
+
+
+    //===============VARIAVEIS IMAGE NAME===========================
     let imgMet = 'Meteoro.png';
     let imgNav = 'frente.png';
     let imgFun = 'fundo.png';
     let imgBala = 'Meteoro.png';
-    let enemyNav =  'frente.png';
+    let imgEnemyBala = 'Meteoro.png';
+    let enemyNav = 'frenteEnemy.png';
 
+
+    //===============VARIAVEIS IMAGE INSTANCE===========================
     let navObj = new Image();
     let metObj = new Image();
     let funObj = new Image();
     let balaObj = new Image();
+    let balaEnemyObj = new Image();
     let enemyObj = new Image();
 
+    //===============VARIAVEIS ARRAYS OBJETOS==========================
     let cometaObj = [];
     let municaoObj = [];
+    let municaoEnemyObj = [];
     let fundoObj = [];
     let enemyNavObj = [];
 
+    //===============VARIAVEIS IMAGENS SRC===========================
     navObj.src = imgNav;
     metObj.src = imgMet;
     funObj.src = imgFun;
     balaObj.src = imgBala;
     enemyObj.src = enemyNav;
+    balaEnemyObj.src = imgEnemyBala;
+    
 
-
+//==========================EVENTOS DE CLICK=========================================
     document.querySelector('body').addEventListener('keydown', function(event){
         notPress = false;
         tecla = event.keyCode;
     });
+
+
+//========================iNICIALIZAÇÃO OBJETOS======================================
+
+    //===============OBJETO NAVE===========================
     function nav(x, y, image, life){
         this.x = x;
         this.y = y;
@@ -56,18 +73,66 @@ start.addEventListener('click', StartGame);
         this.life = life;
 
     }
-    nav.prototype.showI = function(){
-        ctx.drawImage(this.image, this.x, this.y,90, 90);
+
+    //===============OBJETO NAVE INIMIGA===========================
+    function navEnemy(x, y, image, velenemyX, velenemyY, scalenemyX, scalenemyY){
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        this.velenemyX = velenemyX;
+        this.velenemyY = velenemyY;
+        this.scalenemyX = scalenemyX;
+        this.scalenemyY = scalenemyY;
     }
+
+    //===============OBJETO PLANO DE FUNDO===========================
+    function Fundo(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    //===============OBJETO COMETAS===========================
+    function cometas(x, y, velY, velX, scale, isColled){
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.scale = scale;
+        this.isColled = isColled;
+        
+    }
+
+    //===============OBJETO BALA/MUNIÇÃO===========================
+    function bala(x, y, touch){
+        this.x = x;
+        this.y = y;
+        this.touch = touch;
+     
+    }
+
+    function balaEnemy(x, y){
+        this.x = x;
+        this.y = y;
+
+     
+    }
+
+
+
+//=========================UPDATES DOS OBJETOS==========================================
+
+    //===============UPDATE NAVE LIFE===========================
     nav.prototype.Life = function(){
         ctx.fillStyle = 'brown';
-        ctx.fillRect(230,620,800,20 );
+        ctx.fillRect(230,620,500,20 );
         ctx.fillStyle = 'green';
         ctx.fillRect(230,620,this.life,20 );
     }
 
+    //===============UPDATE NAVE PROPERTIES===========================
     nav.prototype.update = function(){
-        
+        ctx.drawImage(this.image, this.x, this.y,90, 90);
+       
         if(tecla== 32){
             let muni = new bala(jogo.x + 40,jogo.y,false);
             municaoObj.push(muni);
@@ -89,33 +154,39 @@ start.addEventListener('click', StartGame);
         if(tecla== 40){
         this.y +=10
         tecla = null;
-    };
+        }
        
     }
-    function Fundo(x, y){
-        this.x = x;
-        this.y = y;
-    }
-    function cometas(x, y, velY, velX, scale, isColled){
-        this.x = x;
-        this.y = y;
-        this.velX = velX;
-        this.velY = velY;
-        this.scale = scale;
-        this.isColled = isColled;
-        
-    }
+  
+    //===============UPDATE FUNDO PROPERTIES===========================
     Fundo.prototype.update = function(){
         this.y +=2;
-        ctx.drawImage(funObj, this.x, this.y, 1280, 645);
+        ctx.drawImage(funObj, this.x, this.y, 1000, 660);
     }
+
+    //===============UPDATE NAVE ENEMY PROPERTIES===========================
+    navEnemy.prototype.update = function(){
+        this.y += this.velenemyY;
+        this.x += this.velenemyX;
+
+       
+        if(municaoEnemyObj.length< 1){
+            ativarBalaEnemy(this);
+        }
+        if(this.x < 0 || this.x > 950)this.velenemyX *= -1;
+        
+        ctx.drawImage(this.image, this.x, this.y, this.scalenemyX, this.scalenemyY);
+        
+    }
+
+    //===============UPDATE COMETAS PROPERTIES===========================
     cometas.prototype.update  = function(){
         
             this.y += this.velY;
             this.x += this.velX
-            if((this.x+10 >= jogo.x && this.x+10<=jogo.x+80)&&(this.y + this.scale>=jogo.y  && this.y<=jogo.y + 80)){
+            if((this.x >= jogo.x+10 && this.x+this.scale-25<=jogo.x+70)&&(this.y + this.scale-20>=jogo.y  && this.y<=jogo.y + 70)){
                 this.isColled = true;
-                jogo.life-=160;
+                jogo.life-=100;
                 if(jogo.life <= 0){
                     gameActive = false;
                     cv.style.display="none";
@@ -125,25 +196,10 @@ start.addEventListener('click', StartGame);
             
 
             ctx.drawImage(metObj,this.x,this.y,this.scale,this.scale);
-    
+          
     }
 
-    function ativarCometa(){
-        if(gameActive){
-            let met = new cometas(Math.floor(Math.random() * 1280, 0),-250, Math.floor(Math.random() * 6)+3,Math.floor(Math.random() * 2)-1,Math.floor(Math.random() * 150)+100, false);
-            cometaObj.push(met);
-            setTimeout('ativarCometa()', '1500');
-        }
-    }
-
-
-    function bala(x, y, touch){
-        this.x = x;
-        this.y = y;
-        this.touch = touch;
-     
-    }
-
+    //===============UPDATE BALA PROPERTIES===========================
     bala.prototype.update = function(){
         for(let j = 0; j<cometaObj.length; j++){
             if((this.x >= cometaObj[j].x+ 5 && this.x<=cometaObj[j].x+ cometaObj[j].scale)&&(this.y>=cometaObj[j].y && this.y<=cometaObj[j].y + cometaObj[j].scale)){
@@ -160,15 +216,100 @@ start.addEventListener('click', StartGame);
         ctx.drawImage(balaObj,this.x ,this.y, 10, 10);
         this.y -= 5;
     }
+
+    balaEnemy.prototype.update = function(){
+      
+        ctx.drawImage(balaEnemyObj,this.x ,this.y, 10, 10);
+        this.y += 5;
+    }
+
+
+//=========================ATIVAÇÃO OBJETOS==========================================
+
+    //===============ATIVAR COMETAS===========================
+    function ativarCometa(){
+        if(gameActive){
+            let met = new cometas(Math.floor(Math.random() * 1000, 0),-250, Math.floor(Math.random() * 6)+3,Math.floor(Math.random() * 2)-1,Math.floor(Math.random() * 150)+100, false);
+            cometaObj.push(met);
+            setTimeout('ativarCometa()', '1500');
+        }
+    }
+
+    //===============ATIVAR NAVE ENEMY===========================
+    function ativarNavEnemy(){
+        if(gameActive){
+            let met = new navEnemy(Math.floor(Math.random() * 1000, 0),-250,enemyObj,4, 1,80, -80);
+            enemyNavObj.push(met);
+            setTimeout('ativarNavEnemy()', '25000');
+        }
+    }
+
+    function ativarBalaEnemy(object){
+        if(object != null){
+            let muni = new balaEnemy(object.x+40,object.y,false);
+            municaoEnemyObj.push(muni);
+            setTimeout('ativarBalaEnemy()', '100');
+        }
+    }
+
+ 
+    //===============UI===========================
+    function Distance(){
+       distancia +=1;
+       setTimeout('Distance()', '1000');
+    }
     function PontosUI(){
         ctx.font = "30px Arial white";
         ctx.fillText('Pontos: '+balaPontos+'.', 30, 30);
+        ctx.font = "30px Arial white";
+        ctx.fillText('Distancia: '+distancia+'.', 30, 70);
     }
 
+//=========================ATIVAÇÃO DOS UPDATES==========================================
+    function AllUpdates(){
+       
+       fundoObj.forEach((fundo)=>{
+            fundo.update();
+            if(fundo.y >=660){
+                fundo.y = -660;
+            }
+        });
+       cometaObj.forEach((cometa)=>{
+            cometa.update();
+            if(cometa.y>=660 || cometa.isColled){
+                cometaObj.splice(cometaObj.indexOf(cometa),1);
+            }
+        });
+         enemyNavObj.forEach((enemy)=>{
+            enemy.update();
+            if(enemy.y >=660){
+                enemyNavObj.splice(enemyNavObj.indexOf(enemy), 1);
+            }
+        });
+          municaoObj.forEach((municao)=>{
+            municao.update();
+            if(municao.y <= 0 || municao.touch == true){
+                municaoObj.splice(municaoObj.indexOf(municao),1);
+            }
+        });
+
+        municaoEnemyObj.forEach((municaoEnemy)=>{
+            municaoEnemy.update();
+            console.log(municaoEnemyObj.length);
+            if(municaoEnemy.y >= 660 ){
+                municaoEnemyObj.splice(municaoEnemyObj.indexOf(municaoEnemy),1);
+            }
+        });
+        
+       
+
+    }
+
+
+//========================="LOOPING" PRINCIPAL==========================================
     function Game(){
         if(gameActive){
         AllUpdates();
-        jogo.showI();
         jogo.update();
         jogo.Life();
        
@@ -176,42 +317,21 @@ start.addEventListener('click', StartGame);
         window.requestAnimationFrame(Game);
         } 
     }
-    
-    function AllUpdates(){
-        for(let k = 0; k<fundoObj.length; k++){
-            fundoObj[k].update();
-            console.log(k);
-            if(fundoObj[k].y >=645){
-                fundoObj[k].y = -645;
-            }
-        }
-        for(let j = 0; j<cometaObj.length; j++){
-            cometaObj[j].update();
-            if(cometaObj[j].y >=630 || cometaObj[j].isColled){
-                cometaObj.splice(cometaObj.indexOf(cometaObj[j]), 1);
-            }
-        }
 
-        for(let i = 0; i<municaoObj.length; i++){
-            municaoObj[i].update();
-            if(municaoObj[i].y <= 0 || municaoObj[i].touch == true){
-                municaoObj.splice(municaoObj.indexOf(municaoObj[i], 1));
-            }
-          
-        }
-    }
-
+//=========================ATIVAÇÃO DO LOOPING PRINCIPAL==========================================
     function StartGame(){
        // let dv = document.querySelector('.game');
         //dv.style.display="none";
         start.style.display="none";
         cv.style.display="block";
         let fd = new Fundo(0, 0);
-        let fdU = new Fundo(0,-645);
+        let fdU = new Fundo(0,-660);
         fundoObj.push(fd);
         fundoObj.push(fdU);
-        jogo = new nav(640, 500, navObj,800);
-        jogo.showI();
+        jogo = new nav(400, 500, navObj,500);
+    
         ativarCometa();
+        ativarNavEnemy();
+        Distance();
         Game();
     }
